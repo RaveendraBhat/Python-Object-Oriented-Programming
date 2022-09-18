dc290c06f0e99fb1bf0a074f9e8e187e0daacf70
RaveendraBhat <98217423+RaveendraBhat@users.noreply.github.com>
2022-09-18 19:41:06 +0530

    Add files via upload

---
+++ aggregation_2.py
@@ -0,0 +1,31 @@
+class Customer:
+    
+    def __init__(self,name,gender,address):
+        self.name = name
+        self.gender = gender
+        self.address = address
+
+    def edit_profile(self,new_name,new_city,new_pin,new_state):
+        self.name = new_name
+        self.address.change_address(new_city,new_pin,new_state)
+
+class Address:
+
+    def __init__(self,city,pincode,state):
+        self.city = city
+        self.pincode = pincode
+        self.state = state
+
+    def change_address(self,new_city,new_pin,new_state):
+        self.city = new_city
+        self.pincode = new_pin
+        self.state = new_state
+
+add = Address('Bengaluru',560066,'Karnataka')
+cust = Customer('Ravi','Male',add)
+
+print(cust.address.city)
+
+cust.edit_profile('Rama','Buffalo',14260,'NewYork')
+
+print(cust.address.city)
